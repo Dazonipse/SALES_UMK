536ca7f36469e365c92577a6e5b7328e6ecb2ea1
Ingrid Palacios <analista3.guma@gmail.com>
2016-06-30 11:43:00 -0600

    Anexo de BD

---
+++ application/views/contenido/administrador/GruposUsuarios.php
@@ -0,0 +1,169 @@
+<!---->
+
+  <section id="content">
+
+    <!--breadcrumbs start-->
+            <div id="breadcrumbs-wrapper" class=" grey lighten-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col s12 m12 l12">
+                            <h5 class="center breadcrumbs-title">USUARIOS</h5> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--breadcrumbs end-->
+               <!--start container-->
+            <div class="container">
+                <div id="invoice">
+                    <div class="invoice-header">
+                        <div class="row section">
+                            
+                         
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="selectores">
+                      <div class="row">
+                        <p>  Grupos de Usuarios</p>
+                        <div class="col s3">
+                          
+                              <ul class="collapsible" data-collapsible="accordion">
+                                  <li>
+                                    <div class="collapsible-header">Grupo de usuario 1</div>
+                                      <div class="collapsible-body">
+                                        <table class="bordered">
+                                          <tr>
+                                            <th>Nombre de usuario</th>
+                                            <th>ruta</th>
+                                          </tr>
+
+                                           <tbody>
+                                             <tr>
+                                               <td>Juanito</td>
+                                               <td>F01</td>
+                                             </tr>
+
+                                             <tr>
+                                               <td>Perenciano</td>
+                                               <td>F02</td>
+                                             </tr>
+                                           </tbody>
+                                        </table>
+                                      </div>
+                                  </li>
+                                  <li>
+                                      <div class="collapsible-header">Grupo de Usuario 2</div>
+                                        <div class="collapsible-body">
+                                              <table class="bordered">
+                                          <tr>
+                                            <th>Nombre de usuario</th>
+                                            <th>ruta</th>
+                                          </tr>
+
+                                           <tbody>
+                                             <tr>
+                                               <td>Juanito</td>
+                                               <td>F01</td>
+                                             </tr>
+
+                                             <tr>
+                                               <td>Perenciano</td>
+                                               <td>F02</td>
+                                             </tr>
+                                           </tbody>
+                                        </table>
+                                      </div>                                      
+                                        </div>
+                                  </li>
+                                 
+                              </ul>
+
+                        </div>
+                      </div>
+                        
+                    </div>
+
+
+                    <div class="invoice-table">
+                        <div class="row">
+                            <div class="col s12 m12 l12">
+                              <p class="center">LISTA DE USUARIOS</p>
+                                <table class="striped">
+                                    <thead>
+                                    <tr>
+                                        <th >FECHA</th>
+                                        <th >RUTA</th>
+                                        <th >CLIENTE</th>
+                                        <th >MONTO</th>
+                                        <th>TOTAL</th>
+                                        <th>SALDO</th>
+
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    <tr>
+                                        <td>29/06/2016</td>
+                                        <td>F4</td>
+                                        <td>CL00256</td>
+                                        <td>C$ 500</td>
+                                        <td>C$ 1000</td>
+                                        <td>C$ 500</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>29/06/2016</td>
+                                        <td>F4</td>
+                                        <td>CL00256</td>
+                                        <td>C$ 500</td>
+                                        <td>C$ 1000</td>
+                                        <td>C$ 500</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>29/06/2016</td>
+                                        <td>F4</td>
+                                        <td>CL00256</td>
+                                        <td>C$ 500</td>
+                                        <td>C$ 1000</td>
+                                        <td>C$ 500</td>
+                                    </tr>
+                                    
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <!--<div class="invoice-footer">
+                        <div class="row">
+                            <div class="col s12 m6 l6">
+                                <p class="strong">Payment Method</p>
+                                <p>Please make the cheque to: AMANDA ORTON</p>
+                                <p class="strong">Terms & Condition</p>
+                                <ul>
+                                    <li>You know, being a test pilot isn't always the healthiest business in the world.</li>
+                                    <li>We predict too much for the next year and yet far too little for the next 10.</li>
+                                </ul>
+                            </div>
+                            <div class="col s12 m6 l6 center-align">
+                                <p>Approved By</p>
+                               <img src="images/signature-scan.png" alt="signature">
+                                <p class="header">AMANDA ORTON</p>
+                                <p>Managing Director</p>
+                            </div>
+                        </div>
+                    </div>-->
+
+                </div>
+            </div>
+            <!--end container-->
+
+
+
+
+
+
+  </section>
